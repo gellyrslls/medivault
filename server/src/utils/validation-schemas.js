20b21de4201c3f83229623aefe3b53b464ccabc7
gelo rosi <22104910@usc.edu.ph>
2024-11-27 13:03:42 +0800

    chore: update dependencies

--- server/src/utils/validation-schemas.js
+++ server/src/utils/validation-schemas.js
@@ -1,54 +1,38 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const registerSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      'string.email': 'Please enter a valid email address',
-      'any.required': 'Email is required'
-    }),
-  password: Joi.string()
-    .min(6)
-    .required()
-    .messages({
-      'string.min': 'Password must be at least 6 characters long',
-      'any.required': 'Password is required'
-    })
+  email: Joi.string().email().required().messages({
+    "string.email": "Please enter a valid email address",
+    "any.required": "Email is required",
+  }),
+  password: Joi.string().min(6).required().messages({
+    "string.min": "Password must be at least 6 characters long",
+    "any.required": "Password is required",
+  }),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      'string.email': 'Please enter a valid email address',
-      'any.required': 'Email is required'
-    }),
-  password: Joi.string()
-    .required()
-    .messages({
-      'any.required': 'Password is required'
-    })
+  email: Joi.string().email().required().messages({
+    "string.email": "Please enter a valid email address",
+    "any.required": "Email is required",
+  }),
+  password: Joi.string().required().messages({
+    "any.required": "Password is required",
+  }),
 });
 
 const changePasswordSchema = Joi.object({
-  currentPassword: Joi.string()
-    .required()
-    .messages({
-      'any.required': 'Current password is required'
-    }),
-  newPassword: Joi.string()
-    .min(6)
-    .required()
-    .messages({
-      'string.min': 'New password must be at least 6 characters long',
-      'any.required': 'New password is required'
-    })
+  currentPassword: Joi.string().required().messages({
+    "any.required": "Current password is required",
+  }),
+  newPassword: Joi.string().min(6).required().messages({
+    "string.min": "New password must be at least 6 characters long",
+    "any.required": "New password is required",
+  }),
 });
 
 module.exports = {
   registerSchema,
   loginSchema,
-  changePasswordSchema
+  changePasswordSchema,
 };
