50110e588e33ef9debc0752fb0065e1f925bc014
gelo rosi <22104910@usc.edu.ph>
2024-12-07 12:38:34 +0800

    feat: implement auth context

---
+++ client/src/context/auth/context.ts
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+import type { LoginCredentials, RegisterCredentials, AuthState } from "@/types";
+
+interface AuthContextType extends AuthState {
+  login: (credentials: LoginCredentials) => Promise<void>;
+  register: (credentials: RegisterCredentials) => Promise<void>;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
